5013ac237a22a837f04ef05467da913552aa1a30
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-13 14:05:53 +0100

    built basic ORM models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^3m7$*-e=pzmd6yu(+76p_2$c)&iy1rd^ydn-jc)9yuq0-kmxc'
+DEFAULT_SECRET_KEY = 'django-insecure-^3m7$*-e=pzmd6yu(+76p_2$c)&iy1rd^ydn-jc)9yuq0-kmxc'
+SECRET_KEY = os.environ.get("SECRET_KEY", DEFAULT_SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+print("ALLOWED HOSTS", os.environ.get("ALLOWED_HOSTS"))
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(',')
 
 
 # Application definition
@@ -37,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'podcast_app',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
---
+++ backend/podcast_app/api/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Language(models.Model):
+    name = models.charField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}".capitalize()
+
+class Podcast(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ManyToManyField(Author)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.language})"
+
+class Episode(models.Model):
+    title = models.CharField(max_length=200)
+    episode_number = models.PositiveIntegerField()
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    duration = models.CharField(max_length=10)
+    listen_link = models.URLField()
+    is_new = models.BooleanField()
+    
+    def __str__(self):
+        return f"{self.episode_number}: {self.title[:15]}... ({self.podcast})"
