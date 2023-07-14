4e0f8c50ae9b5061f58d9eff33ea3f52d19c467b
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-14 16:14:36 +0100

    added trailing slashes to URLs

--- backend/podcast_app/urls.py
+++ backend/podcast_app/urls.py
@@ -4,9 +4,9 @@ from .views import  LanguageList, LanguageDetail, AuthorList, AuthorDetail
 
 
 urlpatterns = [
-    path('language', LanguageList.as_view()),
+    path('language/', LanguageList.as_view()),
     path('language/<int:pk>', LanguageDetail.as_view()),
-    path('author', AuthorList.as_view()),
+    path('author/', AuthorList.as_view()),
     path('author/<int:pk>', AuthorDetail.as_view())
     ]
     
