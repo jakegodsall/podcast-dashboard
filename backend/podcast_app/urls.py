bce0a79732593256b72ac767ba12d33c34dd4c64
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-14 18:10:54 +0100

    added views for Podcast model

--- backend/podcast_app/urls.py
+++ backend/podcast_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from .views import  LanguageList, LanguageDetail, AuthorList, AuthorDetail
+from .views import  LanguageList, LanguageDetail, AuthorList, AuthorDetail, PodcastList, PodcastDetail
 
 
 urlpatterns = [
     path('language/', LanguageList.as_view()),
     path('language/<int:pk>', LanguageDetail.as_view()),
     path('author/', AuthorList.as_view()),
-    path('author/<int:pk>', AuthorDetail.as_view())
-    ]
+    path('author/<int:pk>', AuthorDetail.as_view()),
+    path('podcast/', PodcastList.as_view()),
+    path('podcast/<int:pk>', PodcastDetail.as_view())
+]
     
--- backend/podcast_app/views.py
+++ backend/podcast_app/views.py
@@ -5,7 +5,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import LanguageSerializer, AuthorSerializer
+from .serializers import LanguageSerializer, AuthorSerializer, PodcastSerializer, EpisodeSerializer
 from .models import Language, Author, Podcast, Episode
 
 
@@ -26,7 +26,9 @@ class LanguageList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LanguageDetail(APIView):
-    """Retrieve, update, or delete a Language instance"""
+    """
+    Retrieve, update, or delete a Language instance
+    """
     def get(self, request, pk, format=None):
         language = get_object_or_404(Language, pk=pk)
         serializer = LanguageSerializer(language)
@@ -46,7 +48,9 @@ class LanguageDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
     
 class AuthorList(APIView):
-    """List all authors, or create a new author"""
+    """
+    List all authors, or create a new author
+    """
     def get(self, request, format=None):
         authors = Author.objects.all()
         serializer = AuthorSerializer(authors, many=True)
@@ -61,8 +65,9 @@ class AuthorList(APIView):
     
 
 class AuthorDetail(APIView):
-    """Retrieve, update, or delete an Author instance."""
-
+    """
+    Retrieve, update, or delete an Author instance.
+    """
     def get(self, request, pk, format=None):
         author = get_object_or_404(Author, pk=pk)
         serializer = AuthorSerializer(author)
@@ -80,3 +85,38 @@ class AuthorDetail(APIView):
         author = get_object_or_404(Author, pk=pk)
         author.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class PodcastList(APIView):
+    """
+    List all podcasts, or create a new instance.
+    """
+    def get(self, request, format=None):
+        podcasts = Podcast.objects.all()
+        serializer= PodcastSerializer(podcasts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, format=None):
+        serializer = PodcastSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PodcastDetail(APIView):
+    """
+    Retrieve, update, or delete a Podcast instance
+    """
+    def get(self, request, pk, format=None):
+        podcast = get_object_or_404(Podcast, pk=pk)
+        serializer = PodcastSerializer(podcast)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        podcast = get_object_or_404(podcast, pk=pk)
+        serializer = PodcastSerializer(podcast, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
