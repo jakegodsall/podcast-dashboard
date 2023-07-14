c9567c77f89780c1f55cbe81a4514bd3d1107ee1
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-14 18:10:38 +0100

    added related_name options to relationships in models

--- backend/podcast_app/models.py
+++ backend/podcast_app/models.py
@@ -15,8 +15,8 @@ class Author(models.Model):
 
 class Podcast(models.Model):
     name = models.CharField(max_length=200)
-    author = models.ManyToManyField(Author, blank=True)
-    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    author = models.ManyToManyField(Author, blank=True, related_name='podcast')
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='podcast')
 
     def __str__(self):
         return f"{self.name} ({self.language})"
@@ -24,7 +24,7 @@ class Podcast(models.Model):
 class Episode(models.Model):
     title = models.CharField(max_length=200)
     episode_number = models.PositiveIntegerField()
-    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE, related_name='episode')
     duration = models.CharField(max_length=10)
     listen_link = models.URLField()
     is_new = models.BooleanField(default=False)
