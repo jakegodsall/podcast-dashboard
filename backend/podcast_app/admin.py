d8c7d897edf0b5fcddea4f83e96316b29fab3045
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-14 18:10:16 +0100

    added models to admin site

--- backend/podcast_app/admin.py
+++ backend/podcast_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Language, Author, Podcast, Episode
+
+admin.site.register(Language)
+admin.site.register(Author)
+admin.site.register(Podcast)
+admin.site.register(Episode)
