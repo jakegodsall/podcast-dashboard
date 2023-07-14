878ee3745c01285370a46f4a51ce100c9818c929
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-14 16:52:40 +0100

    Added all serializer classes

--- backend/podcast_app/serializers.py
+++ backend/podcast_app/serializers.py
@@ -45,3 +45,53 @@ class AuthorSerializer(serializers.Serializer):
         return instance
     
 
+class PodcastSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=200)
+    author = AuthorSerializer()
+    language = LanguageSerializer()
+
+    def create(self, validated_data):
+        """
+        Create and return a new Podcast instance, given the validated data.
+        """
+        return Podcast.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing Podcast instance, given the validated data.
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.author = validated_data.get('author', instance.author)
+        instance.language = validated_data.get('language', instance.language)
+
+        instance.save()
+        return instance
+    
+
+class EpisodeSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=200)
+    episode_number = serializers.IntegerField(min_value=0)
+    podcast = PodcastSerializer()
+    duration = serializers.CharField(max_length=10)
+    listen_link = serializers.URLField()
+    in_new = serializers.BooleanField(default=False)
+
+    def create(self, validated_data):
+        """
+        Create a new Episode instance, given the validated data.
+        """
+        return Episode.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing Episode instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.episode_number = validated_data.get('episode_number', instance.episode_number)
+        instance.podcast = validated_data.get('podcast', instance.podcast)
+        instance.duration = validated_data.get('duration', instance.duration)
+        instance.listen_link = validated_data.get('listen_link', instance.listen_link)
+        instance.is_new = validated_data.get('is_new', instance.is_new)
+
+        instance.save()
+        return instance
