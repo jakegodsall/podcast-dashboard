2977097db1163c4141c7e8a2d91a13dc6a61953a
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-16 10:53:47 +0100

    added forwarding through all pages of podcasts

--- scraper/scraper/spiders/polski_daily.py
+++ scraper/scraper/spiders/polski_daily.py
@@ -4,10 +4,10 @@ import scrapy
 class PolskiDailySpider(scrapy.Spider):
     name = "polski_daily"
     allowed_domains = ["www.polskidaily.eu"]
-    start_urls = ["https://www.polskidaily.eu/course/"]
+    start_urls = ["https://www.polskidaily.eu/podcasts-recourse/"]
 
     def parse(self, response):
-        podcasts = response.css('div.podcast-content')
+        podcasts = response.xpath("/div[@class='podcast-content']")
 
         for podcast in podcasts:
             textContent = podcast.css('//h2/a/text()').get()
@@ -20,3 +20,9 @@ class PolskiDailySpider(scrapy.Spider):
             }
 
             yield data
+
+        # go through all pages
+        next_page_url = response.xpath("//ul[@class='page-numbers']//li//a[@class='next page-numbers']").attrib['href']
+        if next_page_url is not None:
+            print(next_page_url)
+            yield response.follow(next_page_url, callback=self.parse)
